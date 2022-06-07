1fe2f202874b4d3e10bc0850b761e7ecf71bec8b
erginbayer <merginbayer@gmail.com>
2022-06-07 13:25:41 +0300

    mvc-tutorial day 1

---
+++ MVCTutorial/MVCTutorial/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCTutorial.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public string GetName()
+        {
+            return "Mehmet Bayer";
+        }
+    }
+}
