1d27acc9a1e39f428038ae7a150f71d47b9c3468
erginbayer <merginbayer@gmail.com>
2022-06-08 11:27:16 +0300

    update

--- MVCTutorial/MVCTutorial/Controllers/HomeController.cs
+++ MVCTutorial/MVCTutorial/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MVCTutorial.Models;
 
 namespace MVCTutorial.Controllers
 {
@@ -6,7 +7,28 @@ namespace MVCTutorial.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            List<BlogPost> blogPosts = new List<BlogPost>();
+
+            BlogPost blogPost1 = new BlogPost();
+            blogPost1.Title = "Paşa Döner'de alım var!";
+            blogPost1.SubTtitle = "Paşa Döner sahibi Yunus Emre Bey yazılımcı departmanına alım yapacağını duyurdu.";
+            blogPost1.PostDate = DateTime.Now;
+
+            BlogPost blogPost2 = new BlogPost();
+            blogPost2.Title = "Köfteci Yusuf'ta alım var!";
+            blogPost2.SubTtitle = "Köfteci Yusuf sahibi Sami Bey yazılımcı departmanına alım yapacağını duyurdu.";
+            blogPost2.PostDate = DateTime.Now;
+
+            BlogPost blogPost3 = new BlogPost();
+            blogPost3.Title = "Tavuk Dünyası'nda alım var!";
+            blogPost3.SubTtitle = "Yavuk Dünyası sahibi Abdullah Bey yazılımcı departmanına alım yapacağını duyurdu.";
+            blogPost3.PostDate = DateTime.Now;
+
+            blogPosts.Add(blogPost1);
+            blogPosts.Add(blogPost2);
+            blogPosts.Add(blogPost3);
+
+            return View(blogPosts);
         }
         public IActionResult About()
         {
@@ -16,7 +38,10 @@ namespace MVCTutorial.Controllers
         {
             return View();
         }
-
+        public IActionResult Post()
+        {
+            return View();
+        }
         public string GetName()
         {
             return "Mehmet Bayer";
---
+++ MVCTutorial/MVCTutorial/Models/BlogPost.cs
@@ -0,0 +1,9 @@
+﻿namespace MVCTutorial.Models
+{
+    public class BlogPost
+    {
+        public string Title { get; set; }
+        public string SubTtitle { get; set; }
+        public DateTime PostDate { get; set; }
+    }
+}
