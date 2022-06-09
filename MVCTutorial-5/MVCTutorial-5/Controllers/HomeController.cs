a7bcd79a5d9ad5326c90341b3e3e93a4e7253620
erginbayer <merginbayer@gmail.com>
2022-06-09 11:26:56 +0300

    Bootstrap Example

---
+++ MVCTutorial-5/MVCTutorial-5/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCTutorial_5.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult Price()
+        {
+            return View();
+        }
+        public IActionResult Blog()
+        {
+            return View();
+        }
+        public IActionResult Service()
+        {
+            return View();
+        }
+        public IActionResult Single()
+        {
+            return View();
+        }
+    }
+}
