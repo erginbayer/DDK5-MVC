48b8ab74f5bf818afee9c9019f6149eb6f037ca8
erginbayer <merginbayer@gmail.com>
2022-06-20 14:34:54 +0300

    n-tier sample

---
+++ dotnet-mvc-ntier-sample/SiemensECommerce.Business/Manager/CategoryManager.cs
@@ -0,0 +1,40 @@
+﻿using SiemensECommerce.Data.ORM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiemensECommerce.Business.Manager
+{
+    public class CategoryManager
+    {
+        public List<Category> GetCategories()
+        {
+            SiemensECommerceContext context = new SiemensECommerceContext();
+            var categories = context.Categories.ToList();
+            return categories;
+        }
+        public Category GetCategoryById(int id)
+        {
+            SiemensECommerceContext context = new SiemensECommerceContext();
+            var category = context.Categories.FirstOrDefault(c => c.Id == id);
+            return category;
+        }
+
+        public void Add(Category category)
+        {
+            SiemensECommerceContext context = new SiemensECommerceContext();
+            context.Categories.Add(category);
+            context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            SiemensECommerceContext context = new SiemensECommerceContext();
+            var category = context.Categories.FirstOrDefault(c => c.Id == id);
+            category.IsDeleted = true;
+            context.SaveChanges();
+        }
+    }
+}
---
+++ dotnet-mvc-ntier-sample/SiemensECommerce.UI/Controllers/CategoryController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SiemensECommerce.Business.Manager;
+using SiemensECommerce.Data.ORM;
+
+namespace SiemensECommerce.UI.Controllers
+{
+    public class CategoryController : Controller
+    {
+        SiemensECommerceContext context = new SiemensECommerceContext();
+        public IActionResult Index()
+        {
+            CategoryManager categoryManager = new CategoryManager();    
+            var categories = categoryManager.GetCategories();
+            //var categories = context.Categories.ToList();
+            return View(categories);
+        }
+    }
+}
